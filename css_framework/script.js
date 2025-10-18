700dcd2de849423a38411a86749770883cc3ce81
Giovanni Franco <giovannifranco2000@gmail.com>
2025-10-19 00:32:47 +0200

    added css framework functionalities

--- css_framework/script.js
+++ css_framework/script.js
@@ -1,17 +1,51 @@
+const root = document.documentElement;
+
+function updateSizeUnit() {
+    const vw = window.innerWidth / 100;
+    const vh = window.innerHeight / 100;
+
+    const vmin = Math.min(vw, vh);
+    const vmax = Math.max(vw, vh);
+
+    const size = (vmax / vmin) + (vmin / vmax);
+    root.style.setProperty("--object-size-unit", size + "px");
+}
+
+let resizeTimer;
+const THROTTLE_TIME_MS = 100;
+
+window.addEventListener("resize", () => {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        updateSizeUnit();
+    }, THROTTLE_TIME_MS);
+});
+
+window.addEventListener("orientationchange", () => {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        updateSizeUnit();
+    }, THROTTLE_TIME_MS);
+});
+
+updateSizeUnit();
+
 window.addEventListener("DOMContentLoaded", () => {
 
     document.querySelectorAll(".hoverable").forEach((element) => {
 
+        // since width is fixed, will only make the element "jump"
         element.addEventListener("mouseenter", (event) => {
-            elementHeight = event.target.offsetHeight;
+            let elementHeight = event.target.offsetHeight;
             event.target.style.height = (elementHeight * 1.2).toString() + "px";
         });
 
+        // since width is fixed, will only make the element "jump"
         element.addEventListener("mouseleave", (event) => {
-            elementHeight = event.target.offsetHeight;
+            let elementHeight = event.target.offsetHeight;
             event.target.style.height = (elementHeight / 1.2).toString() + "px";
         })
-        
+
     });
 
 });
